67303ea7e49dbe7edd5a4fffe3cded7eb9c5ae9e
Carlos Tadeu <carlossantos@opus-software.com.br>
2023-03-10 17:42:04 -0300

    adding consumer

---
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,5 @@
+
+?
+settings.gradle,0/5/05efc8b1657769a27696d478ded1e95f38737233
+I
+kafka-basics/build.gradle,e/3/e3654e87ff6ebd41529074e5b68a7b019084903f
---
+++ kafka-basics/src/main/java/kafka/ConsumerDemo.java
@@ -0,0 +1,52 @@
+package kafka;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Collections;
+import java.util.Properties;
+
+public class ConsumerDemo {
+
+    private static final Logger log = LoggerFactory.getLogger(ConsumerDemo.class.getSimpleName());
+
+    public static void main(String[] args) {
+        log.info("I am a Kafka Consumer");
+
+        String bootstrapServers = "127.0.0.1:9092";
+        String groupId = "my-second-application";
+        String topic = "demo_java";
+
+        // create consumer configs
+        Properties properties = new Properties();
+        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+        properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+
+        // create consumer
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(properties);
+
+        // subscribe consumer to out topic(s)
+        consumer.subscribe(Collections.singleton(topic));
+
+        // poll for new data
+        while (true) {
+            log.info("Polling");
+
+            ConsumerRecords<String, String> records =
+                    consumer.poll(1000);
+
+            for (ConsumerRecord<String, String> record : records) {
+                log.info(String.format("Key: %s, Value: %s", record.key(), record.value()));
+                log.info(String.format("Partition: %s, Offset: %s", record.partition(), record.offset()));
+            }
+        }
+    }
+}
